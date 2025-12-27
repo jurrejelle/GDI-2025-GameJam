e351df872ba726397b7464e30eaa2831e86c061e
jurrejelle <jurre@jilles.com>
2025-12-27 09:55:00 +0100

    First bits of NavMesh, collision, agent

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,4 +1,7 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem.Android;
 
 public class Enemy : MonoBehaviour
 {
@@ -6,15 +9,26 @@ public class Enemy : MonoBehaviour
     public float hitpoints = 100f;
 
     public Transform lookAtPlayer;
+    public Transform goal;
+
+    private NavMeshAgent agent;
+
+    private bool isGoalSet = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = gameObject.AddComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isGoalSet)
+        {
+            agent.SetDestination(goal.position);
+            isGoalSet = true;
+        }
+
         Vector3 dir = lookAtPlayer.position - transform.position;
         dir.y = 0;
 
@@ -29,6 +43,16 @@ public class Enemy : MonoBehaviour
         {
             Destroy(gameObject);
             EnemyManger.Get().Kill(this);
+        }
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision entered");
+        if (other.gameObject.CompareTag("HouseTrigger"))
+        {
+            Debug.Log("YOU DIE!!!!!");
         }
     }
 }
--- Assets/Scripts/Enemy/EnemyManger.cs
+++ Assets/Scripts/Enemy/EnemyManger.cs
@@ -12,28 +12,24 @@ public class EnemyManger : MonoBehaviour
         { World.Fantasy, new List<GameObject>() }
     };
 
+    private Dictionary<World, Transform> _goalsPerWorld;
+    public Dictionary<World, Transform> GoalsPerWorld => _goalsPerWorld ??= new()
+    {
+        { World.Scifi, GameObject.Find("/Goals/World_2").transform},
+        { World.Western, GameObject.Find("/Goals/World_2").transform},
+        { World.Fantasy, GameObject.Find("/Goals/World_2").transform},
+    };
+
     private static EnemyManger INSTANCE;
 
-    private Transform player;
-    private Transform enemyParent;
-
-    private Transform GetPlayer()
-    {
-        if (player == null)
-        {
-            player = GameManager.Get().getPlayer().transform;
-        }
-        return player;
-    }
+    private Transform _enemyParent;
+    private Transform EnemyParent => _enemyParent ??= GameObject.Find("/Enemies").transform;
     
-    private Transform GetEnemyParent()
-    {
-        if (enemyParent == null)
-        {
-            enemyParent = GameObject.Find("/Enemies").transform;
-        }
-        return enemyParent;
-    }
+    
+    private Transform _player;
+    private Transform Player => _player ??= GameManager.Get().getPlayer().transform;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -67,11 +63,13 @@ public class EnemyManger : MonoBehaviour
                 Vector3 start = GameManager.Get().WorldOffsets[world];
                 Vector2 offsetInUnitCircle = Random.insideUnitCircle * 10;
                 Vector3 offset = new Vector3(offsetInUnitCircle.x, 1f, offsetInUnitCircle.y);
-                var newEnemy = Instantiate(GameManager.Get().GetCurrentEnemyPrefab(), start + offset,
+                var newEnemyObject = Instantiate(GameManager.Get().GetCurrentEnemyPrefab(), start + offset,
                     Quaternion.identity);
-                newEnemy.GetComponent<Enemy>().lookAtPlayer = GetPlayer();
-                newEnemy.transform.SetParent(GetEnemyParent(), true);
-                Enemies.Add(newEnemy);
+                var newEnemy = newEnemyObject.GetComponent<Enemy>();
+                newEnemy.lookAtPlayer = Player;
+                newEnemy.goal = GoalsPerWorld[world];
+                newEnemyObject.transform.SetParent(EnemyParent, true);
+                Enemies.Add(newEnemyObject);
             }
         }
     }
